570bb740d36af4531733e710f0039c89b3223467
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-19 13:49:07 +0600

    Add VideoClip class extending TimelineClip

---
+++ lib/features/editor/application/video_clip.dart
@@ -0,0 +1,19 @@
+import 'package:digitor/features/editor/domain/models/timeline_clip.dart';
+
+class VideoClip extends TimelineClip {
+  const VideoClip({
+    required super.id,
+    required super.start,
+    required super.duration,
+    required this.path,
+
+    super.position,
+    super.scale,
+    super.rotation,
+    super.opacity,
+    super.visible,
+    super.locked,
+  });
+
+  final String path;
+}
