4e38d985066fe0414a987f9b71de770467e13b72
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-19 14:05:19 +0600

    Create timeline_constants.dart

---
+++ lib/features/editor/presentation/timeline/timeline_constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TimelineConstants {
+  static const double rulerHeight = 32;
+
+  static const double trackHeight = 72;
+
+  static const double headerWidth = 80;
+
+  static const double pixelsPerSecond = 80;
+
+  static const double clipRadius = 10;
+
+  static const EdgeInsets clipPadding =
+      EdgeInsets.symmetric(horizontal: 6);
+
+  static const double playheadWidth = 2;
+}
