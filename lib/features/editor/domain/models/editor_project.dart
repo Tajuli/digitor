cb31d0aa00a7c7ec08619c3f649ffe21e5a712ae
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-19 13:45:29 +0600

    Add EditorProject class with properties and copyWith method

---
+++ lib/features/editor/domain/models/editor_project.dart
@@ -0,0 +1,41 @@
+import 'package:digitor/features/editor/domain/models/timeline_track.dart';
+import 'package:flutter/material.dart';
+
+class EditorProject {
+  const EditorProject({
+    required this.tracks,
+    required this.duration,
+
+    this.fps = 30,
+
+    this.canvasSize = const Size(1080, 1920),
+
+    this.background = Colors.black,
+  });
+
+  final List<TimelineTrack> tracks;
+
+  final Duration duration;
+
+  final int fps;
+
+  final Size canvasSize;
+
+  final Color background;
+
+  EditorProject copyWith({
+    List<TimelineTrack>? tracks,
+    Duration? duration,
+    int? fps,
+    Size? canvasSize,
+    Color? background,
+  }) {
+    return EditorProject(
+      tracks: tracks ?? this.tracks,
+      duration: duration ?? this.duration,
+      fps: fps ?? this.fps,
+      canvasSize: canvasSize ?? this.canvasSize,
+      background: background ?? this.background,
+    );
+  }
+}
