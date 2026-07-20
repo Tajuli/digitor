1dcf7bb4896938e5ec00866143071e837127074e
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-20 22:00:58 +0600

    Fix editor toolbar syntax and project initialization

--- lib/features/editor/domain/models/editor_project.dart
+++ lib/features/editor/domain/models/editor_project.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class EditorProject {
   EditorProject({
-    required this.tracks,
+    required List<TimelineTrack> tracks,
     required this.duration,
 
     this.fps = 30,
