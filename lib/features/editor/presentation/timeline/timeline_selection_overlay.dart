cdf07c751ffd51fd0c2f044c8b1179a3fca20da5
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-23 13:23:47 +0600

    Delete lib directory

--- lib/features/editor/presentation/timeline/timeline_selection_overlay.dart
+++
@@ -1,2 +0,0 @@
-import 'package:flutter/material.dart';
-class TimelineSelectionOverlay extends StatelessWidget { const TimelineSelectionOverlay({super.key, required this.onClear}); final VoidCallback onClear; @override Widget build(BuildContext context) => Positioned.fill(child: GestureDetector(behavior: HitTestBehavior.translucent, onTap: onClear)); }
