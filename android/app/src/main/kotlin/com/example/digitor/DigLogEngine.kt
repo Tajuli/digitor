aa707525d6320767ee0ca13791b23b779b315631
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-24 00:22:47 +0600

    Delete android directory

--- android/app/src/main/kotlin/com/example/digitor/DigLogEngine.kt
+++
@@ -1,9 +0,0 @@
-package com.example.digitor
-
-interface DigLogEngine {
-    val actualBitDepth: Int
-    val codecName: String
-    fun start()
-    fun stop(): Boolean
-    fun outputIsValid(): Boolean
-}
