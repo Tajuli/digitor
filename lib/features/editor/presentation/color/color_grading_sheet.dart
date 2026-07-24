3c64e3ec96f2bc18ec79281fe310e3fe37be01ac
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-07-25 01:23:19 +0600

    Update color_grading_sheet.dart

--- lib/features/editor/presentation/color/color_grading_sheet.dart
+++ lib/features/editor/presentation/color/color_grading_sheet.dart
@@ -777,7 +777,6 @@ class _CurveEditorState extends State<_CurveEditor> {
           },
           onPanUpdate: (details) => _moveActive(details.localPosition, size),
           onPanEnd: (_) => _activePoint = null,
-          onPanCancel: () => _activePoint = null,
           onLongPressStart: (details) {
             final position = _normalized(details.localPosition, size);
             final next = [...points, position]..sort((a, b) => a.dx.compareTo(b.dx));
