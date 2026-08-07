e3d281d84fc2af75cba9c2d82416caf4e10dbb4f
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-08 00:38:09 +0600

    Initialize DigitorEngine before app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:digitor/app/digitor_app.dart';
+import 'package:digitor/core/engine/digitor_engine_runtime.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  DigitorEngineRuntime.instance.initialize();
   runApp(const DigitorApp());
 }
