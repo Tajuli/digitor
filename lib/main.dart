17dd51bfec78cb80a3ef85d1bed7444c040727e9
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-09 14:15:22 +0600

    Initialize DigitorEngine before UI startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:digitor/app/digitor_app.dart';
+import 'package:digitor/core/engine/digitor_engine_gateway.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DigitorEngineGateway.instance.initialize();
   runApp(const DigitorApp());
 }
