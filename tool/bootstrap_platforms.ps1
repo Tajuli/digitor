04384049ad5763a42397c82c0685d995aad3e8ee
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-12 23:43:45 +0600

    Delete tool directory

--- tool/bootstrap_platforms.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-Write-Host 'Generating Digitor platform hosts...'
-flutter config --enable-windows-desktop
-flutter create --platforms=android,windows --project-name digitor .
-flutter pub get
-
-Write-Host 'Android and Windows hosts are ready.'
-Write-Host 'iOS/macOS hosts must be generated on macOS with tool/bootstrap_apple.sh.'
