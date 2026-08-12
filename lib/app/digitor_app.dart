4a8a541b9ec7974df06245a453cf0980c1099051
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-13 00:39:03 +0600

    Use startup-safe DigitorEngine gateway

--- lib/app/digitor_app.dart
+++ lib/app/digitor_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../core/engine/bootstrapped_engine_gateway.dart';
 import '../core/engine/engine_gateway.dart';
-import '../core/engine/ffi_engine_gateway.dart';
 import '../features/editor/presentation/editor_screen.dart';
 
 class DigitorApp extends StatefulWidget {
@@ -12,7 +12,7 @@ class DigitorApp extends StatefulWidget {
 }
 
 class _DigitorAppState extends State<DigitorApp> {
-  late final EngineGateway _engine = DigitorFfiEngineGateway();
+  late final EngineGateway _engine = BootstrappedDigitorEngineGateway();
 
   @override
   Widget build(BuildContext context) {
