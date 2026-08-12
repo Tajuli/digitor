16b5f6d60e8f8c070413535290ae4771b8556348
Tajuli <40664071+Tajuli@users.noreply.github.com>
2026-08-13 00:39:14 +0600

    Move DigitorEngine bootstrap behind app gateway

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
-import 'package:digitor_engine_ffi/digitor_engine_ffi.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'app/digitor_app.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  final bootstrap = await DigitorFlutterProductionBootstrap.resolve();
-  if (kDebugMode) {
-    debugPrint(
-      '[DigitorEngine] bootstrap platform=${bootstrap.platform} '
-      'textureHostReady=${bootstrap.textureHostReady} '
-      'productionHostRegistered=${bootstrap.productionHostRegistered} '
-      'ready=${bootstrap.ready} '
-      'diagnostic=${bootstrap.diagnostic}',
-    );
-  }
-
   runApp(const DigitorApp());
 }
